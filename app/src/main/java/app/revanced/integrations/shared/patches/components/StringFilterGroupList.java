5b99ffe065c2be6ecde68943cdd9b379186c9444
Aykhan <ayxanshikhaliyevuv@gmail.com>
2024-11-08 12:36:13 +0400

    Remove for fixing

--- app/src/main/java/app/revanced/integrations/shared/patches/components/StringFilterGroupList.java
+++
@@ -1,9 +0,0 @@
-package app.revanced.integrations.shared.patches.components;
-
-import app.revanced.integrations.shared.utils.StringTrieSearch;
-
-public final class StringFilterGroupList extends FilterGroupList<String, StringFilterGroup> {
-    protected StringTrieSearch createSearchGraph() {
-        return new StringTrieSearch();
-    }
-}
